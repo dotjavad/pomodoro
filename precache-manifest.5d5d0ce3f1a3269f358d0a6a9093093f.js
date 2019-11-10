ff73ace085d07c798beff1f0243d65d7378aabc8
Javad <mirjavad@webtamin.com>
2019-11-10 18:58:22 +0330

    Updates

---
+++ precache-manifest.5d5d0ce3f1a3269f358d0a6a9093093f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a053acfa6010852334db5b78c340d6",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "009e49de33bd4b5f60c4",
+    "url": "/pomodoro/static/css/2.2a2afa88.chunk.css"
+  },
+  {
+    "revision": "acf7c073b3c1602ce60e",
+    "url": "/pomodoro/static/css/main.4347a56f.chunk.css"
+  },
+  {
+    "revision": "009e49de33bd4b5f60c4",
+    "url": "/pomodoro/static/js/2.9f7ac1b5.chunk.js"
+  },
+  {
+    "revision": "acf7c073b3c1602ce60e",
+    "url": "/pomodoro/static/js/main.3c537d9b.chunk.js"
+  },
+  {
+    "revision": "2b06fd74cafc1de73872",
+    "url": "/pomodoro/static/js/runtime-main.4bd1b737.js"
+  },
+  {
+    "revision": "d71fb1e6f8f6d8f4685c6725d6839444",
+    "url": "/pomodoro/static/media/alarm.d71fb1e6.mp3"
+  }
+]);
